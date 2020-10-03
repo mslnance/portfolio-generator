5ec26c89183b09f9f2c7d668317c3c9f3de3aa2b
Symphony <55611595+lnance6@users.noreply.github.com>
2020-10-02 17:30:37 -0700

    added ability to generate a webpage

--- app.js
+++ app.js
@@ -1,16 +1,12 @@
-const profileDataArgs=process.argv.slice(2,process.argv.length);
-console.log(profileDataArgs);
+const fs = require('fs');
+const generatePage = require('./src/page-template.js');
 
-const printProfileData= profileDataArr => {
-    //This...
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
+const profileDataArgs = process.argv.slice(2);
 
-    console.log('================');
+const [name, github] = profileDataArgs;
 
-    // Is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+fs.writeFile('./index.html', generatePage(name, github), err => {
+  if (err) throw new Error(err);
 
-printProfileData(profileDataArgs)
+  console.log('Portfolio complete! Check out index.html to see the output!');
+});
